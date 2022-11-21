c80dee38340acdcef0232e4f9049c026f0606ed3
Adam Robson <adamray312@gmail.com>
2022-11-20 23:32:33 -0800

    style search

--- src/App.js
+++ src/App.js
@@ -10,52 +10,43 @@ const API_URL = 'http://www.omdbapi.com/?apikey=9a5fc751';
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-
   const movieSearch = async (title) => {
-    const res = await fetch(`${API_URL}&s=${title}`);
+    const res = await fetch(`${ API_URL }&s=${ title }`);
     const data = await res.json();
     setMovies(data.Search);
   };
-
   useEffect(() => {
     movieSearch(searchTerm);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-
   return (
     <section className="App">
-      <h1>Find a Movie</h1>
+      <header>
+        <h2 className="title">find movies</h2>
+      </header>
       <div className="search">
         <input
+          className="search-input"
           value={ searchTerm }
           placeholder="search"
           type="text"
           onChange={ (e) => setSearchTerm(e.target.value) }
         />
         <img
+          className="icon"
           src={ searchIcon }
           alt="search"
           onClick={ (e) => movieSearch(searchTerm) }
         />
-
         <div className="container">
-
+          { movies?.length > 0 ?
+            (<div className="movies">{
+              movies.map(
+                (movie) => (
+                  <MovieCard key='key' movie={ movie } />))}</div>) :
+            (<div className="empty">search results appear here</div>)
+          }
         </div>
-
-
-        {
-          movies?.length > 0 ?
-            (
-              <div className="container">{
-                movies.map((movie) => (<MovieCard key='key' movie={ movie } />))
-              }</div>
-            )
-            :
-            (
-              <div className="empty">No movies found</div>
-            )
-        }
       </div>
     </section>
   );
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './MovieCard.css';
 
 const Moviecard = ({ movie }) => {
   return (
